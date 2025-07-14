0b32b91f11054248f22a5f19fcd108bbb4835265
guiohm79 <guiohm79@gmail.com>
2025-07-14 17:33:26 +0200

    fix

--- melody-sequencer/app/lib/randomEngine.js
+++ melody-sequencer/app/lib/randomEngine.js
@@ -20,7 +20,7 @@ export const SCALES = {
   phrygian:         [0,1,3,5,7,8,10],
   phrygianDominant: [0,1,4,5,7,8,10],
   dorian:           [0,2,3,5,7,9,10],
-  perso:            [0,3,7,8],        // gamme perso 1‑3‑7‑8
+  perso:            [0,4,10,12],        // gamme perso 1‑3‑7‑8
 };
 
 // Renvoie toutes les notes de la gamme sur une plage d’octaves
